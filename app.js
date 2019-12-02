e5931221706b573a15be557c8f699a9e13c60e9b
MelvinManni <melvinnnamani@gmail.com>
2019-12-02 07:57:59 +0100

    new loading effect

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
+var loadIcon = document.getElementById('loader');
+var resultSpan = document.getElementById('result-span');
 
-document.getElementById('submit').addEventListener("click",multiplfunc);
+loadIcon.style.display = 'none';
+resultSpan.style.display = 'none';
+document.getElementById('submit').addEventListener('click',function(e){
+        if (num1.value === "" || num2.value === "") {
+                alert("Enter a valid number!");
+        }
+        else{
+                resultSpan.style.display = 'none';
+                loadIcon.style.display = 'block';
+
+                setTimeout(multiplfunc, 1500)
+        }
+        e.preventDefault();
+});
 
 function multiplfunc(){
         if (num1.value === "" || num2.value === "") {
@@ -11,7 +26,10 @@ function multiplfunc(){
 
                 var mult = parseFloat(num1.value) * parseFloat(num2.value);
                 document.getElementById('result').innerHTML = mult;
+                resultSpan.style.display = 'block';
+                loadIcon.style.display = 'none';
+
         }
 
-        e.preventDefault();
+        
  }
